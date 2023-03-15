31675062da1e6aa4ea8d31e54d8b331d9c441be8
WesleyRobsonGalisa <127518332+WesleyRobsonGalisa@users.noreply.github.com>
2023-03-15 11:26:12 -0300

    Update Calculadora.java
    Agora temos melhorias na calculadora, ignorando numeros e letras que não sejam o que esperamos.

--- Calculadora.java
+++ Calculadora.java
@@ -17,64 +17,92 @@ public class Calculadora{
             System.out.println("3 Multiplicar");
             System.out.println("4 Dividir");
             escolha = input.nextInt();
+            System.out.print("\f");
             
                 switch(escolha){
                     case 1: 
+                    System.out.println("SOMAR:");    
                     System.out.println("digite o número");
                     num1 = input.nextDouble();
                     System.out.println("digite o outro número");
                     num2 = input.nextDouble();
                     resultado = num1+num2;
                     System.out.println("Resultado: "+resultado);
-                    System.out.println("Deseja fazer outra operação?");
-                    System.out.println("1 SIM");
-                    System.out.println("2 NÃO");
-                    escolha2 = input.nextInt();
                     
-                    break;
                     
+                    
+                        do{
+                            System.out.println("Deseja fazer outra operação?");
+                            System.out.println("1 SIM");
+                            System.out.println("2 NÃO");
+                            escolha2 = input.nextInt();
+                            System.out.print("\f");
+                    
+                        }while(escolha2!=1 && escolha2!=2);  
+                        break;
+                        
+                        
                     case 2: 
+                    System.out.println("SUBTRAIR: ");
                     System.out.println("digite o número");
                     num1 = input.nextDouble();
                     System.out.println("digite o outro número");
                     num2 = input.nextDouble();
                     resultado = num1-num2;
                     System.out.println("Resultado: "+resultado);
-                    System.out.println("Deseja fazer outra operação?");
-                    System.out.println("1 SIM");
-                    System.out.println("2 NÃO");
-                    escolha2 = input.nextInt();
                     
-                    break;
+                    
+                         do{
+                            System.out.println("Deseja fazer outra operação?");
+                            System.out.println("1 SIM");
+                            System.out.println("2 NÃO");
+                            escolha2 = input.nextInt();
+                            System.out.print("\f");
+                    
+                        }while(escolha2!=1&&escolha2!=2);
+                        break;
+                    
+                    
                     
                     case 3: 
+                    System.out.println("MULTIPLICAR:");
                     System.out.println("digite o número");
                     num1 = input.nextDouble();
                     System.out.println("digite o outro número");
                     num2 = input.nextDouble();
                     resultado = num1*num2;
                     System.out.println("Resultado: "+resultado);
-                    System.out.println("Deseja fazer outra operação?");
-                    System.out.println("1 SIM");
-                    System.out.println("2 NÃO");
-                    escolha2 = input.nextInt();
                     
-                    break;
+                    
+                         do{
+                            System.out.println("Deseja fazer outra operação?");
+                            System.out.println("1 SIM");
+                            System.out.println("2 NÃO");
+                            escolha2 = input.nextInt();
+                            System.out.print("\f");
+                    
+                        }while(escolha2!=1 && escolha2!=2);
+                        break;
                     
                     case 4: 
+                    System.out.println("DIVIDIR:");
                     System.out.println("digite o número");
                     num1 = input.nextDouble();
                     System.out.println("digite o outro número");
                     num2 = input.nextDouble();
                     resultado = num1/num2;
                     System.out.println("Resultado: "+resultado);
-                    System.out.println("Deseja fazer outra operação?");
-                    System.out.println("1 SIM");
-                    System.out.println("2 NÃO");
-                    escolha2 = input.nextInt();
+                                        
                     
+                        do{
+                            System.out.println("Deseja fazer outra operação?");
+                            System.out.println("1 SIM");
+                            System.out.println("2 NÃO");
+                            escolha2 = input.nextInt();
+                            System.out.print("\f");
                     
-                    break;
+                        }while(escolha2!=1 && escolha2!=2);
+                        break;
                     
                     default: System.out.println("operação não reconhecida");
             }
